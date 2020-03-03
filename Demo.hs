13a8ace42b1b91e6648784ca11e6a7e3f429c3f4
Dmitri Barabanov <dmitri.barabanov@gmail.com>
2020-03-04 01:35:50 +0300

    starting lists

--- Demo.hs
+++ Demo.hs
@@ -41,3 +41,26 @@ helper mem1 mem2 cur n | n > 0 && n==cur = mem1+mem2
 		       | n < 0 && n == cur = mem1-mem2
 		       | n < 0 && n < cur = helper mem2 (mem1-mem2) (cur-1) n
                        | otherwise = undefined
+
+--GHCi> sum'n'count (-39)
+--(12,2)
+sum'n'count :: Integer -> (Integer, Integer)
+sum'n'count x = (sumDigits (abs x), numDigits (abs x))
+  where sumDigits x | x < 10 = x
+                    | otherwise = mod x 10 + sumDigits (div x 10) 
+        numDigits x | x < 10 = 1
+                    | otherwise = 1 + numDigits (div x 10)   
+
+--GHCi> integration sin pi 0
+---2.0
+integration :: (Double -> Double) -> Double -> Double -> Double
+integration f a b = trapeze f a b 1000 where
+  trapeze f a b n = step a b n *((f(a) + f(b))/2 + sumf 0 a 1 n) where
+    sumf acc a i n | i==n = acc
+                   | otherwise = sumf (acc + f(a+i*step a b n)) a (i+1) n
+    step a b n = (b-a)/n
+
+oddsOnly :: Integral a => [a] -> [a]
+oddsOnly [] = []
+oddsOnly (a:as) | odd a = a:oddsOnly as
+                | otherwise = oddsOnly as
